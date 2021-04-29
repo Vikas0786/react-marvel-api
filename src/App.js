c0ef2ee70868b25f64fa674e3a0722252569d95d
Vikas0786 <vthakur412000@gmail.com>
2021-04-29 13:33:50 +0530

    Home Component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Marvel api implementation</h1>
+      <Home />
     </div>
   );
 }
