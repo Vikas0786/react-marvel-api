8ba3dd3e7f016ed8b6cda562bf6b3ec28d935dd9
Vikas0786 <vthakur412000@gmail.com>
2021-04-29 18:02:57 +0530

    API integration done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import "../App.css";
 
-function Home() {
+const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h2>
+        This is a single page application created using React.js and Marvel API.
+      </h2>
+      <h3>This application uses Characters, Comics and Series listings.</h3>
+      <p>
+        Developed by{" "}
+        <a
+          href="https://www.linkedin.com/in/vikasthakur0427/"
+          className="underline"
+          target="blank"
+        >
+          Vikas Thakur
+        </a>
+      </p>
     </div>
   );
-}
+};
 
 export default Home;
